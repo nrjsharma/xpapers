9f3bc7b38b8e2750a407f5092b15701f97d33ec3
nrjsharma <nrjsharma083@gmail.com>
2020-06-28 06:26:08 +0530

    update upload paper

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.views import View
+from dashboard.models import University
 
 
 class DashboardView(View):
+    template_name = 'dashboard/dashboard.html'
+
     def get(self, request, path=None):
-        return render(request, 'dashboard/dashboard.html')
+        context = {
+            'universities': University.objects.all()
+        }
+        return render(request, self.template_name, context)
 
 
 class UploadPaperView(View):
+    template_name = 'dashboard/upload-paper.html'
+
     def get(self, request, path=None):
-        return render(request, 'dashboard/upload-paper.html')
+        return render(request, self.template_name)
