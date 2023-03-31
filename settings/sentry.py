2a5203a3d621fe45204f4c16283a769fd6510244
Neeraj Sharma <nrjsharma083@gmail.com>
2023-03-31 16:17:39 +0530

    Update sentry.py

--- settings/sentry.py
+++ settings/sentry.py
@@ -2,7 +2,7 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 sentry_sdk.init(
-    dsn="https://9f7d2cdb3e154ed8baa685998cd3eab2@o377243.ingest.sentry.io/5374333",
+    dsn="os.getenv('sentry_dsn')",
     integrations=[DjangoIntegration()],
 
     # If you wish to associate users to errors (assuming you are using
